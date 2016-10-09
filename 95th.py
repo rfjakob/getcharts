3325e4322082cd100e1dd5b6d11e8eafdcbd625e
Jakob Unterwurzacher <jakobunt@gmail.com>
2016-10-09 14:23:22 +0200

    Fix array append bug

--- 95th.py
+++ 95th.py
@@ -3,6 +3,11 @@
 import csv
 import datetime
 import numpy
+import os
+import sys
+
+# Change to script directory
+os.chdir(os.path.dirname(sys.argv[0]))
 
 def pct(day_rows):
 	ps = []
@@ -18,11 +23,12 @@ def pct(day_rows):
 
 fd = open('frontpage.txt','rb')
 parsed = csv.reader(fd, delimiter='\t')
-day = 0
-day_rows_empty=[[],[],[],[],[]]
+date = datetime.date(1,1,1)
+month = 0
 outfd = open('95th.txt', 'wb')
 outcsv = csv.writer(outfd, delimiter='\t')
 
+first = True
 for row in parsed:
 	try:
 		unixtime = float(row[0])
@@ -32,13 +38,15 @@ for row in parsed:
 
 	dt = datetime.datetime.utcfromtimestamp(unixtime)
 
-	if dt.day != day:
-		if day > 0:
+	if dt.date() != date:
+		if not first:
 			pct(day_rows)
-		day_rows = day_rows_empty
-		day = dt.day
+		first = False
+		day_rows = [[],[],[],[],[]]
+		date = dt.date()
 
 	for i, val in enumerate(row):
 		f = float(row[i])
+		# Skip NaN
 		if f > 0:
 			day_rows[i].append(f)
